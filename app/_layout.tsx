f40f4d133945d8558b98ad2cc76c0cfd7a6faaf6
izumii <154342465+izumiCode@users.noreply.github.com>
2025-05-17 12:51:33 +0530

    splash screen setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,28 @@
 import InitialLayout from "@/components/initialLayout";
 import ClerkConvexProvider from "@/providers/clerk-convex";
-import React from "react";
+import { useFonts } from "expo-font";
+import { SplashScreen } from "expo-router";
+import React, { useCallback } from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMono-Medium.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
   return (
     <ClerkConvexProvider>
       <SafeAreaProvider>
-        <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
+        <SafeAreaView
+          style={{ flex: 1, backgroundColor: "#000" }}
+          onLayout={onLayoutRootView}
+        >
           <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
